2c1a3b8d3b1c4c0a21b3a3bbec505fd403342f43
lancelote <lancelote.du.lac@gmail.com>
2015-06-20 18:47:29 +0300

    functional tests for new item input

--- lists/tests/acceptance/features/lists.feature
+++ lists/tests/acceptance/features/lists.feature
@@ -4,10 +4,11 @@ Feature: Lists Web App
   Scenario: Simple visit
     When I open an index page
     Then I see a title says "To-Do"
-#    And I am invited to enter "To-DO" item
-#    Then I enter "Buy peacock feathers" into a text box
-#    When I hit enter
-#    Then Page updates and now it lists "1: Buy peacock feathers"
+    And I see header mention "To-Do" lists
+    And I am invited to enter a To-Do item
+    Then I enter "Buy peacock feathers" into a text box
+    When I hit enter
+    Then Page updates and now it lists "1: Buy peacock feathers"
 #    And There is still a text box inviting me to enter another To-Do item
 #    Then I enter "Use peacock feathers to make a fly" into a text box
 #    When I hit enter
--- lists/tests/acceptance/steps/steps.py
+++ lists/tests/acceptance/steps/steps.py
@@ -6,7 +6,12 @@ Acceptance tests
 """
 
 from behave import when, then
-from nose.tools import assert_in
+from nose.tools import (
+    assert_in,
+    assert_equal,
+    assert_true
+)
+from selenium.webdriver.common.keys import Keys
 
 
 @when('I open an index page')
@@ -23,3 +28,52 @@ def step_impl(context, text):
     Check title text
     """
     assert_in(text, context.browser.title)
+
+
+@then('I see header mention "{text}" lists')
+def step_impl(context, text):
+    """
+    Check page header
+    """
+    header_text = context.browser.find_element_by_id('h1').text
+    assert_in(text, header_text)
+
+
+@then('I am invited to enter a To-Do item')
+def step_impl(context):
+    """
+    Check if there is a input box to enter new To-Do item
+    """
+    context.input_box = context.browser.find_element_by_id('id_new_item')
+    assert_equal(
+        context.input_box.get_attribute('placeholder'),
+        'Enter a To-Do item'
+    )
+
+
+@then('I enter "{text}" into a text box')
+def step_impl(context, text):
+    """
+    Input text to input_box
+    """
+    context.input_box.send_keys(text)
+
+
+@when('I hit enter')
+def step_impl(context):
+    """
+    Send input_box content to server
+    """
+    context.input_box.send_keys(Keys.ENTER)
+
+
+@then('Page updates and now it lists "{text}"')
+def step_impl(context, text):
+    """
+    Check if the page show new To-Do item
+    """
+    table = context.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    assert_true(
+        any(row.text == text for row in rows)
+    )
