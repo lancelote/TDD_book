67d6da83e53bb0611d0b65fd0aa69897fbd52c86
lancelote <lancelote.du.lac@gmail.com>
2015-07-11 17:37:15 +0300

    Style error fix

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
-# Turn off (too-many-instance-attributes), (invalid-name) and
-# (missing-docstring) pylint errors:
-# pylint: disable=R0902,C0103,C0111
+# Turn off (too-many-instance-attributes), (invalid-name),
+# (missing-docstring) and (no-member) pylint errors:
+# pylint: disable=R0902,C0103,C0111,E1101
 
 """
 Unit tests
