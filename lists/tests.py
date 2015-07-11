2003540e205c9f94cb284647f111098465f8231d
lancelote <lancelote.du.lac@gmail.com>
2015-07-11 17:04:04 +0300

    new url and view for adding to existing lists

--- lists/tests.py
+++ lists/tests.py
@@ -70,8 +70,8 @@ class ListViewTest(TestCase):
         Item.objects.create(text='Item 1', list=correct_list)
         Item.objects.create(text='Item 2', list=correct_list)
         another_list = List.objects.create()
-        Item.objects.create(test='Another Item 1', list=another_list)
-        Item.objects.create(test='Another Item 2', list=another_list)
+        Item.objects.create(text='Another Item 1', list=another_list)
+        Item.objects.create(text='Another Item 2', list=another_list)
 
         response = self.client.get('/lists/{0}/'.format(correct_list.id))
 
@@ -79,6 +79,14 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'Item 2')
         self.assertNotContains(response, 'Another Item 1')
         self.assertNotContains(response, 'Another Item 2')
+
+    def test_passes_correct_list_to_template(self):
+        _ = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.get('/lists/{0}/'.format(correct_list.id))
+
+        self.assertEqual(response.context['list'], correct_list)
 
 
 class NewListTest(TestCase):
@@ -98,5 +106,34 @@ class NewListTest(TestCase):
             '/lists/new',
             data={'item_text': 'A new list item'}
         )
+        new_list = List.objects.first()
 
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, '/lists/{0}/'.format(new_list.id))
+
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        _ = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/{0}/add_item'.format(correct_list.id),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        _ = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/{0}/add_item'.format(correct_list.id),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/{0}/'.format(correct_list.id))
--- lists/views.py
+++ lists/views.py
@@ -18,8 +18,8 @@ def view_list(request, list_id):
     """
     Returns list of To-Do items
     """
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+    lst = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': lst})
 
 
 def new_list(request):
@@ -28,4 +28,13 @@ def new_list(request):
     """
     lst = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=lst)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect('/lists/{0}/'.format(lst.id))
+
+
+def add_item(request, list_id):
+    """
+    Adds new item to a existing list
+    """
+    lst = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=lst)
+    return redirect('/lists/{0}/'.format(lst.id))
