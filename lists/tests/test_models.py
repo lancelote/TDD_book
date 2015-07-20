932937b5b7345b88b4e66188717e76984ae1b97b
Pavel Karateev <lancelote.du.lac@gmail.com>
2015-07-20 15:15:47 +0300

    Split unit tests into two files

---
+++ lists/tests/test_models.py
@@ -0,0 +1,41 @@
+# Turn off (too-many-instance-attributes), (invalid-name),
+# (missing-docstring) and (no-member) pylint errors:
+# pylint: disable=R0902,C0103,C0111,E1101
+
+"""
+Models unit tests
+"""
+
+from django.test import TestCase
+
+from lists.models import Item, List
+
+
+class ListAndItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        lst = List()
+        lst.save()
+
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = lst
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = lst
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, lst)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, lst)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, lst)
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,7 +3,7 @@
 # pylint: disable=R0902,C0103,C0111,E1101
 
 """
-Unit tests
+Views unit tests
 """
 
 from django.core.urlresolvers import resolve
@@ -26,36 +26,6 @@ class HomePageTest(TestCase):
         response = homepage(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
-
-
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        lst = List()
-        lst.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = lst
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = lst
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, lst)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, lst)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, lst)
 
 
 class ListViewTest(TestCase):
