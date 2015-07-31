d2086674d2202b32498b3c5af51cafc621e61b3e
Pavel Karateev <lancelote.du.lac@gmail.com>
2015-07-31 13:32:42 +0300

    Wait for document to be ready before JS

--- lists/static/js/list.js
+++ lists/static/js/list.js
@@ -1,11 +1,13 @@
-var input = $('input');
+jQuery(document).ready(function ($) {
+    var input = $('input');
 
-// Hide error on input click
-input.click(function () {
-    $('.has-error').hide();
-});
+    // Hide error on input click
+    input.click(function () {
+        $('.has-error').hide();
+    });
 
-// Hide error on keypress
-input.on('keypress', function () {
-    $('.has-error').hide();
+    // Hide error on keypress
+    input.on('keypress', function () {
+        $('.has-error').hide();
+    });
 });
